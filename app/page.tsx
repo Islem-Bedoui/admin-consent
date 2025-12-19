dd80b998f69e0afc084c6c06787816b162ef9469
Islem Bedoui <islem.bedoui@almakom.ch>
2025-12-19 17:51:36 +0100

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+      <h1>Merci, le consentement a été accepté&nbsp;!</h1>
+      <p>Vous pouvez fermer cette page.</p>
+    </main>
+  );
+}
